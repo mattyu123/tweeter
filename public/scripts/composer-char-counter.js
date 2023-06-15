86133d8a5b281ed300cd4b2590aa1a1784edddf3
mattyu123 <matthewyu100@gmail.com>
2023-06-15 18:22:29 -0400

    change the counter text to red if it's negative

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,9 +2,20 @@ $(document).ready(function() {
   console.log("your js file is successfully linked")
 });
 
+//Counter for how many characters the user has left
 $('#tweet-text').on("input", function() {
-  const total = $(this).val()
+  const total = $(this).val();
+  const maxCharacters = 140;
+  const lengthOfTweet = total.length;
+  const charactersRemaining = maxCharacters - lengthOfTweet
 
-  console.log(total)
-  console.log(total.length)
+  //traverse up the DOM tree and back down again looking for .counter class
+  $(this).parents().find(".counter").val(charactersRemaining)
+
+  const parent = $(this).parents().find(".counter")
+
+  if (charactersRemaining < 0) {
+    $(parent).css("color", "red")
+  }
 })
+
