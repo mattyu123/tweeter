03656bf42ed579787119682b9d8c20a459bf9cb7
mattyu123 <matthewyu100@gmail.com>
2023-06-19 18:32:59 -0400

    create function that appends tweets in array to #tweets-container

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,4 +1,4 @@
-const data =
+const data = [
   {
     "user": {
       "name": "Newton",
@@ -8,19 +8,29 @@ const data =
     "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-    "created_at": 1687037192188
+    "created_at": 1687038785789
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1687125185789
   }
+]
 
 //Takes a tweet object and returns a tweet article element containing entire HTML structure of tweet
 const createTweetElement= function(tweet) {
   let $tweet = (
   `<article>
     <header class = "tweet-header">
-      <div>
+      <div class="face-name">
         <i class="fa-solid fa-face-smile"></i>
-        <h3>
-          ${tweet.user.name}
-        </h3>
+        <h3>${tweet.user.name}</h3>
       </div>
       <h3 id="user-handle">${tweet.user.handle}</h3>
     </header>
@@ -38,10 +48,13 @@ const createTweetElement= function(tweet) {
   </article>
   `);
   return $tweet;
+};
+
+//render function takes in an array of tweet objects and appends each to #tweets-container 
+const renderTweets = function(tweetArr) {
+  for (let item = 0; item < tweetArr.length; item++) {
+    $('#tweets-container').append(createTweetElement(tweetArr[item]))
+  }
 }
 
-const $tweet = createTweetElement(data)
-
-console.log($tweet)
-
-$('.tweets-container').append($tweet)
+renderTweets(data)
